2f23b264dccb8ed0fcc470632aadff4e1c508eba
Einha Muday <132117926+einhaalliahgmuday@users.noreply.github.com>
2024-11-14 23:49:11 +0800

    changed folder name to "printmed"

--- printmed/src/components/Settings.jsx
+++ printmed/src/components/Settings.jsx
@@ -1,7 +1,7 @@
 
 import { Outlet } from 'react-router-dom';
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 const Settings = ({ children }) => {
   return (
