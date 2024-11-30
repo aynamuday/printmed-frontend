eae5c12ea0bb326a4668557037245bca98bbd73b
Nico Ampoloquio <116412203+NicoAmpoloquio@users.noreply.github.com>
2024-11-30 17:20:51 +0800

    Update EditAdminInfoPage.jsx

--- printmed/src/pages/EditAdminInfoPage.jsx
+++ printmed/src/pages/EditAdminInfoPage.jsx
@@ -205,20 +205,6 @@ const EditAdminInfoPage = () => {
                   required
                 />
               </div>
-
-              {/* Email */}
-              <div>
-                <label className="block text-sm font-medium test-gray-700">
-                  Email
-                </label>
-                <input 
-                  type="email" 
-                  name="email" value={formData.email} 
-                  onChange={handleChange} 
-                  className="mt-1 block w-full border border-gray-500 rounded-md shadow-sm p-2"
-                  readOnly
-                />
-              </div>
             </div>
 
             <div className="mt-8 w-full">
