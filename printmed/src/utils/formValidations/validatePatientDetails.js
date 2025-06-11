122741237e00efb2a0645cf4aa171c7b68c4e286
Nico Ampoloquio <nicoampoloquio2003@gmail.com>
2025-06-11 20:53:44 +0800

    Update validatePatientDetails.js

--- printmed/src/utils/formValidations/validatePatientDetails.js
+++ printmed/src/utils/formValidations/validatePatientDetails.js
@@ -7,7 +7,7 @@ export const validatePatientDetails = (e, setErrors, setFormData, formData) => {
     const capitalizedValue = name !== "email" && name !== "suffix" && name !== "payment_method" && name !== "hmo" ? capitalizedWords(value) : value
 
     // should not accept numbers and special characters    
-    if ((name === 'first_name' || name === 'middle_name' || name === 'last_name') && /[^a-zA-Z\s]/.test(value)) {
+    if ((name === 'first_name' || name === 'middle_name' || name === 'last_name') && /[^a-zA-ZñÑ\s]/.test(value)) {
         setErrors((prevErrors) => ({
             ...prevErrors,
             [name]: 'Cannot contain numbers or special characters.',
@@ -16,7 +16,7 @@ export const validatePatientDetails = (e, setErrors, setFormData, formData) => {
     }
 
     if (name === "birthplace") {
-        if (!/^([a-zA-Z][a-zA-Z, ]*|)$/.test(value)) {
+        if (!/^([a-zA-ZñÑ][a-zA-ZñÑ, ]*|)$/.test(value)) {
             setErrors((prevErrors) => ({
                 ...prevErrors,
                 [name]: 'Can only contain letters and comma, and must start with a letter.',
@@ -42,7 +42,7 @@ export const validatePatientDetails = (e, setErrors, setFormData, formData) => {
       
     // no symbols allowed
     if (name === 'house_number') {
-        if (/[^a-zA-Z0-9\s]/.test(value)) {
+        if (/[^a-zA-ZñÑ0-9\s]/.test(value)) {
             setErrors((prevErrors) => ({
                 ...prevErrors,
                 [name]: 'Cannot contain special characters.',
@@ -66,7 +66,7 @@ export const validatePatientDetails = (e, setErrors, setFormData, formData) => {
         const emailUsername = value.toLowerCase();
 
         // only allows letters, numbers, dot
-        if (!/^[a-zA-Z0-9.]*$/.test(emailUsername)) {
+        if (!/^[a-zA-ZñÑ0-9.]*$/.test(emailUsername)) {
             setErrors((prevErrors) => ({...prevErrors, email: "Can only contain letters, numbers, and dot."}))
             return
         }
